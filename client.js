612ebf7b9d817dfd41be1715d09b767d505c9ac3
Reyn <jl88744653@gmail.com>
2016-08-09 18:37:53 +0800

    take client as an function

--- client.js
+++ client.js
@@ -2,74 +2,82 @@
  // 引入readline模块
 var readline = require('readline');
 var WebSocketClient = require('websocket').client;
-var client = new WebSocketClient();
 
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
-});
+var Client = function () {
+    var client = new WebSocketClient();
 
-client.on('connect', function(connection) {
-    console.log('WebSocket client connected');
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
+    client.on('connectFailed', function(error) {
+        console.log('Connect Error: ' + error.toString());
     });
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-        connection.close();
-        process.exit(0);
-    });
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log("\nReceived: '" + message.utf8Data + "'");
-        }
-    });
-    query(connection);
-});
 
-var getTimeStamp = function() {
-    var date = new Date();
-    var timeStamp = Date.parse(date) / 1000;
-    return timeStamp;
-};
-
-var query = function(connection) {
-    //创建readline接口实例
-    var roomId = process.argv[2] || 0;
-    var userId = process.argv[3] || 0;
-
-    console.log('roomId, userId: ', roomId, userId);
-
-    var rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    var actions = [
-        'createRoom',
-        'joinRoom',
-        'exitRoom',
-        'dismissRoom',
-        'roomInfo'
-    ];
-    return function repeat() {
-        rl.question("\n发送：", function(answer) {
-            if (answer.length > 0) {
-                var action = 'sendMessage';
-                var index = actions.indexOf(answer);
-                if (typeof index == 'number' && index >= 0) {
-                    action = answer;
-                }
-                var request = {
-                    'action': action,
-                    'roomId': roomId,
-                    'userId': userId,
-                    'message': answer.toString(),
-                    'dateline': getTimeStamp()
-                };
-                connection.sendUTF(JSON.stringify(request));
-            }
-            repeat();
+    client.on('connect', function(connection) {
+        console.log('WebSocket client connected');
+        connection.on('error', function(error) {
+            console.log("Connection Error: " + error.toString());
         });
-    }();
+        connection.on('close', function() {
+            console.log('echo-protocol Connection Closed');
+            connection.close();
+            process.exit(0);
+        });
+        connection.on('message', function(message) {
+            if (message.type === 'utf8') {
+                console.log("\nReceived: '" + message.utf8Data + "'");
+            }
+        });
+        query(connection);
+    });
+
+    var getTimeStamp = function() {
+        var date = new Date();
+        var timeStamp = Date.parse(date) / 1000;
+        return timeStamp;
+    };
+
+    var query = function(connection) {
+        //创建readline接口实例
+        var roomId = process.argv[2] || 0;
+        var userId = process.argv[3] || 0;
+
+        console.log('roomId, userId: ', roomId, userId);
+
+        var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        var actions = [
+            'createRoom',
+            'joinRoom',
+            'exitRoom',
+            'dismissRoom',
+            'roomInfo'
+        ];
+        return function repeat() {
+            rl.question("\n发送：", function(answer) {
+                if (answer.length > 0) {
+                    var action = 'sendMessage';
+                    var index = actions.indexOf(answer);
+                    if (typeof index == 'number' && index >= 0) {
+                        action = answer;
+                    }
+                    var request = {
+                        'action': action,
+                        'roomId': roomId,
+                        'userId': userId,
+                        'message': answer.toString(),
+                        'dateline': getTimeStamp()
+                    };
+                    connection.sendUTF(JSON.stringify(request));
+                }
+                repeat();
+            });
+        }();
+    };
+
+    client.connect('ws://localhost:8888', 'echo-protocol');
 };
 
-client.connect('ws://localhost:8888', 'echo-protocol');
+var num = 1;
+for (var i = 1; i <= num; i++) {
+    new Client();
+}
